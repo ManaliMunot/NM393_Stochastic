620fc6bc5390922595d5a450ed5ad0064258e5ab
Jiten <jitenadav@gmail.com>
2020-08-01 14:06:29 +0530

    Flask Login Module

--- Flask_server/app.py
+++ Flask_server/app.py
@@ -20,3 +20,33 @@ def home():
         return redirect(url_for('uDashboard'))
 
     return render_template('index.html')
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('uDashboard'))
+
+    if request.method == 'POST' :
+        users = mongo.db.users
+        utype = request.form['Login']
+        login_user = users.find_one({'email' : request.form['email']})
+
+        if login_user :
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'),login_user['password']) == login_user['password']:
+                if login_user['utype'] != utype:
+                    flash("Not a User/Admin")
+                    return redirect(url_for('login'))
+
+                if login_user['utype'] == "u":
+                    session['username'] = login_user['uname']
+                    return redirect(url_for('uDashboard',utype = utype))
+                else :
+                    session['username'] = login_user['uname']
+                    return redirect(url_for('adminDashboard',utype = utype))
+
+
+        flash('Invalid Email/Password combination')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
