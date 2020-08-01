e1f400d33694940a29137410fd15c492425faf8d
Jiten <jitenadav@gmail.com>
2020-08-01 17:50:02 +0530

    Building and Compiling model

--- Flask_server/traffic_sign/train.py
+++ Flask_server/traffic_sign/train.py
@@ -45,3 +45,25 @@ print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 #Converting the labels into one hot encoding
 y_train = to_categorical(y_train, 43)
 y_test = to_categorical(y_test, 43)
+
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+#Compilation of the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+model.save("my_model.h5")
