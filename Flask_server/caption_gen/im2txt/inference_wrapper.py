395b81707cb64e6fa3319cce207253cf576f62ba
Jiten <jitenadav@gmail.com>
2020-08-02 10:26:15 +0530

    Inputs

--- Flask_server/caption_gen/im2txt/inference_wrapper.py
+++ Flask_server/caption_gen/im2txt/inference_wrapper.py
@@ -18,3 +18,17 @@ class InferenceWrapper(inference_wrapper_base.InferenceWrapperBase):
     model = show_and_tell_model.ShowAndTellModel(model_config, mode="inference")
     model.build()
     return model
+
+  def feed_image(self, sess, encoded_image):
+    initial_state = sess.run(fetches="lstm/initial_state:0",
+                             feed_dict={"image_feed:0": encoded_image})
+    return initial_state
+
+  def inference_step(self, sess, input_feed, state_feed):
+    softmax_output, state_output = sess.run(
+        fetches=["softmax:0", "lstm/state:0"],
+        feed_dict={
+            "input_feed:0": input_feed,
+            "lstm/state_feed:0": state_feed,
+        })
+    return softmax_output, state_output, None
